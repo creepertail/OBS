99228dc2a978a27ca075efe5d41559e857287e3c
Du-Brandon <asdffhnkl123@gmail.com>
2025-11-12 21:43:16 +0800

    [Refactoring] customize order update dto validation

--- back_end/obs-backend/src/order/dto/update-order.dto.ts
+++ back_end/obs-backend/src/order/dto/update-order.dto.ts
@@ -1,5 +1,42 @@
 // src/order/dto/update-order.dto.ts
-import { PartialType } from '@nestjs/mapped-types';
-import { CreateOrderDto } from './create-order.dto';
+// src/order/dto/update-order.dto.ts
+import { IsInt, IsOptional, IsString, IsUUID, MaxLength, Min } from 'class-validator';
 
-export class UpdateOrderDto extends PartialType(CreateOrderDto) {}
+export class UpdateOrderDto {
+  @IsString()
+  @MaxLength(100)
+  @IsOptional()
+  shippingAddress?: string;
+
+  @IsInt()
+  @Min(0)
+  @IsOptional()
+  paymentMethod?: number;
+
+  @IsInt()
+  @Min(0)
+  @IsOptional()
+  totalPrice?: number;
+
+  @IsInt()
+  @Min(0)
+  @IsOptional()
+  state?: number;
+
+  @IsInt()
+  @Min(1)
+  @IsOptional()
+  totalAmount?: number;
+
+  @IsUUID()
+  @IsOptional()
+  userId?: string;
+
+  @IsUUID()
+  @IsOptional()
+  merchantId?: string;
+
+  @IsUUID()
+  @IsOptional()
+  couponId?: string;
+}
