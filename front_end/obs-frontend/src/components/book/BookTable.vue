65a8aa4c932450fc7a3f8e539ba5fc0a108898cc
joyce <joycehsu427@gmail.com>
2025-12-16 14:25:45 +0800

    [Bug fixing] book table

--- front_end/obs-frontend/src/components/book/BookTable.vue
+++ front_end/obs-frontend/src/components/book/BookTable.vue
@@ -20,13 +20,15 @@ const props = defineProps({
 </script>
 
 <style scoped>
+
 .book-table {
   display: grid;
 
-  /* 自適應列數：每個 item 最小 200px，最大 1fr */
-  grid-template-columns: repeat(auto-fit, minmax(200px, 1fr));
+  /* 關鍵差異在 auto-fill */
+  grid-template-columns: repeat(auto-fill, minmax(200px, 1fr));
 
   gap: 20px;
   padding: 10px 0;
 }
+
 </style>
