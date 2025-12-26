8780a75d5ce824f1397566611a9f03efce391e64
Ottoelif <ianchen611@gmail.com>
2025-12-26 18:20:16 +0800

    [Deletion] Delete the tem folder/

--- back_end/tem/book/dto/update-book.dto.ts
+++
@@ -1,5 +0,0 @@
-// src/book/dto/update-book.dto.ts
-import { PartialType } from '@nestjs/mapped-types';
-import { CreateBookDto } from './create-book.dto';
-
-export class UpdateBookDto extends PartialType(CreateBookDto) {}
